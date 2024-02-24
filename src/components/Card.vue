69867cf06a5c8749f4284a3e23a307ccee4d1898
Ilya Alikin <iljuha.alikin@gmail.com>
2024-02-24 13:12:59 +0500

    added card

---
+++ src/components/Card.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="card">
+    <div class="card__img">
+      <img :src="imageUrl" alt="" />
+      <div class="card__like">
+        <img :src="isFavorite ? '/favorite.svg' : '/unfavorite.svg'" alt="" />
+      </div>
+    </div>
+    <div class="card__description description-card">
+      <div class="description-card__name">
+        {{ name }}
+      </div>
+      <div class="description-card__for">
+        {{ description }}
+      </div>
+      <div class="description-card__price">{{ price }}€</div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+defineProps({
+  imageUrl: String,
+  name: String,
+  description: String,
+  price: Number,
+  isFavorite: Boolean,
+});
+</script>
+<style scoped>
+.card {
+  position: relative;
+}
+
+.card__img {
+  position: relative;
+  background: #f6f6f6;
+  width: 300px;
+  height: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+
+.card__like {
+  position: absolute;
+  top: 5px;
+  left: 5px;
+}
+
+.card__description {
+  display: flex;
+  flex-direction: column;
+  row-gap: 5px;
+}
+.description-card {
+}
+.description-card__name {
+  font-size: 20px;
+  font-weight: 700;
+}
+.description-card__for {
+  font-size: 20px;
+  color: #838383;
+}
+.description-card__price {
+  font-size: 20px;
+  font-weight: 800;
+}
+</style>
--- src/components/NewArrivals.vue
+++ src/components/NewArrivals.vue
@@ -5,10 +5,21 @@
         <h2 class="section__title new-arrivals__title">new arrivals</h2>
         <div class="title-section__link">See all items</div>
       </div>
+      <div class="new-arrivals__slider">
+        <Card
+          imageUrl="/sneakers/sneakers-1.png"
+          name="Air Jordan 1 Retro Low Voodoo"
+          description="Men’s shoes"
+          :price="379.99"
+          :isFavorite="true"
+        />
+      </div>
     </div>
   </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Card from "../components/Card.vue";
+</script>
 <style scoped>
 .new-arrivals {
   margin-top: 100px;
