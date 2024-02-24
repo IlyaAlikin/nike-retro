a73a965c07db3c7a739757fda915d1a25fa3ef49
Ilya Alikin <iljuha.alikin@gmail.com>
2024-02-24 12:14:30 +0500

    added section-title

--- src/components/NewArrivals.vue
+++ src/components/NewArrivals.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="new-arrivals">
-    <h2 class="new-arrivals__title">new arrivals</h2>
+    <div class="new-arrivals__container">
+      <div class="title-section new-arrivals__title-section">
+        <h2 class="section__title new-arrivals__title">new arrivals</h2>
+        <div class="title-section__link">See all items</div>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts" setup></script>
-<style scoped></style>
+<style scoped>
+.new-arrivals {
+  margin-top: 100px;
+}
+</style>
