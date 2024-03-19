fca835642af2aa3d63f8905ef13d1005d3d06b0b
Ilya Alikin <iljuha.alikin@gmail.com>
2024-03-19 12:54:15 +0500

    started adding new arrivals section

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <Header />
   <Members class-card="" />
   <Main />
+  <NewArrivals />
 </template>
 <script lang="ts" setup>
 import Header from "./components/header/Header.vue";
 import Members from "./components/Members.vue";
 import Main from "./pages/Main.vue";
+import NewArrivals from "./pages/NewArrivals.vue";
 </script>
 <style scoped></style>
---
+++ src/pages/NewArrivals.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="new-arrivals">
+    <div class="new-arrivals__title-section">
+      <h1 class="new-arrivals__title">NEW ARRIVALS</h1>
+    </div>
+    <div class="new-arrivals__content">
+      <div class="new-arrivals__filters"></div>
+      <div class="new-arrivals__products"></div>
+    </div>
+  </div>
+</template>
+<script setup></script>
+<style scoped></style>
